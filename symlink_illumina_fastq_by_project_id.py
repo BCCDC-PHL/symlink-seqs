85cd7a8d8fb1377dcffdf3dbbcbb90f39a7207a4
Dan Fornika <dfornika@gmail.com>
2021-11-29 15:23:39 -0800

    Add --copy flag

--- symlink_illumina_fastq_by_project_id.py
+++ symlink_illumina_fastq_by_project_id.py
@@ -5,16 +5,24 @@ import glob
 import json
 import logging
 import os
+import shutil
 import re
 
 
 # https://stackoverflow.com/a/1176023
 def camel_to_snake(name):
-  name = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
-  return re.sub('([a-z0-9])([A-Z])', r'\1_\2', name).lower()
+    """
+    Convert camelCase strings to snake_case strings
+    """
+    name = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', name).lower()
 
 
 def determine_sequencer_type(run_id):
+    """
+    Determine sequencer type based on format of Run ID
+    Returns str ("miseq" or "nextseq")
+    """
     miseq_regex = '\d{6}_M\d{5}_\d{4}_\d{9}-[A-Z0-9]{5}'
     nextseq_regex = '\d{6}_VH\d{5}_\d+_[A-Z0-9]{9}'
     sequencer_type = None
@@ -27,6 +35,9 @@ def determine_sequencer_type(run_id):
 
 
 def parse_samplesheet_miseq(samplesheet_path):
+    """
+    Parse a MiSeq SampleSheet to get sample and project IDs
+    """
     data = []
     with open(samplesheet_path, 'r') as f:
         for line in f:
@@ -51,6 +62,9 @@ def parse_samplesheet_miseq(samplesheet_path):
 
 
 def parse_samplesheet_nextseq(samplesheet_path):
+    """
+    Parse a NextSeq SampleSheet to get sample and project IDs
+    """
     cloud_data_lines = []
     cloud_data = []
     with open(samplesheet_path, 'r') as f:
@@ -79,6 +93,10 @@ def parse_samplesheet_nextseq(samplesheet_path):
 
 
 def has_necessary_fields_for_symlinking_miseq(sample):
+    """
+    Check if a 'sample' dictionary has the necessary fields for symlinking
+    Returns bool (True if ready for symlinking, false otherwise)
+    """
     selected = False
     conditions = []
     if 'sample_name' in sample and sample['sample_name'] != "":
@@ -101,6 +119,10 @@ def has_necessary_fields_for_symlinking_miseq(sample):
 
 
 def has_necessary_fields_for_symlinking_nextseq(sample):
+    """
+    Check if a 'sample' dictionary has the necessary fields for symlinking
+    Returns bool (True if ready for symlinking, false otherwise)
+    """
     selected = False
     conditions = []
     if 'sample_id' in sample and sample['sample_id'] != "":
@@ -119,10 +141,17 @@ def has_necessary_fields_for_symlinking_nextseq(sample):
     return selected
 
 
-def create_symlinks(samples, sequencer_type, run_dir, outdir, simplify_sample_id):
-    if not os.path.exists(outdir):
-        os.makedirs(outdir)
+def get_src_dest_paths(samples, sequencer_type, run_dir, outdir, simplify_sample_id):
+    """
+    samples: List of dicts, with keys "sample_name" OR "sample_id"
+    sequencer_type: str ("miseq" or "nextseq")
+    run_dir: path to illumina sequencer output directory
+    outdir: destination dir to create symlinks or copies
+    simplify_sample_id: bool If True, sample ID will be truncated at first '_' char.
 
+    Returns a list of dicts with keys "src" and "dest"
+    """
+    src_dest = []
     fastq_subdir = None
     if sequencer_type == "miseq":
         fastq_subdir = "Data/Intensities/BaseCalls"
@@ -150,10 +179,36 @@ def create_symlinks(samples, sequencer_type, run_dir, outdir, simplify_sample_id
                         extension += '.gz'
                     dest_filename = dest_filename.split('_')[0] + '_' + read + extension
                 dest = os.path.join(outdir, dest_filename)
-                try:
-                    os.symlink(src, dest)
-                except OSError as e:
-                    print(str(e) + ". sample_id: " + sample_id + ", file: " + dest)
+                src_dest.append({"src": src, "dest": dest})
+
+    return src_dest        
+
+
+def create_symlinks(src_dest_paths):
+    """
+    src_dest_paths: list of dicts with keys "src" and "dest"
+    Symlink src to dest for each element in src_dest_paths
+    """
+    for paths in src_dest_paths:
+        outdir = os.path.dirname(paths['dest'])
+        if not os.path.exists(outdir):
+            os.makedirs(outdir)
+        try:
+            os.symlink(paths['src'], paths['dest'])
+        except OSError as e:
+            print(str(e) + ". files: " + paths)
+
+
+def create_copies(src_dest_paths):
+    """
+    src_dest_paths: list of dicts with keys "src" and "dest"
+    Copy src to dest for each element in src_dest_paths
+    """
+    for paths in src_dest_paths:
+        outdir = os.path.dirname(paths['dest'])
+        if not os.path.exists(outdir):
+            os.makedirs(outdir)
+        shutil.copy(paths['src'], paths['dest'])
 
 
 def main():
@@ -161,6 +216,7 @@ def main():
     parser.add_argument('-r', '--run-dir')
     parser.add_argument('-p', '--project-id')
     parser.add_argument('-s', '--simplify-sample-id', action='store_true')
+    parser.add_argument('-c', '--copy', action='store_true')
     parser.add_argument('-o', '--outdir')
     args = parser.parse_args()
 
@@ -180,7 +236,12 @@ def main():
         candidate_samples = filter(has_necessary_fields_for_symlinking_nextseq, all_samples)
         selected_samples = filter(lambda x: x['project_name'] == args.project_id, candidate_samples)
 
-    create_symlinks(selected_samples, sequencer_type, args.run_dir, args.outdir, args.simplify_sample_id)
+    src_dest_paths = get_src_dest_paths(selected_samples, sequencer_type, args.run_dir, args.outdir, args.simplify_sample_id)
+
+    if args.copy:
+      create_copies(src_dest_paths)
+    else:
+      create_symlinks(src_dest_paths)
 
 
 if __name__ == '__main__':
