dc7979cd24aae830fb1cf28b6366a2c785041af9
Dan Fornika <dfornika@gmail.com>
2026-06-18 16:22:31 -0700

    update test data

--- .github/scripts/download_run_simulator.sh
+++ .github/scripts/download_run_simulator.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-wget https://github.com/dfornika/illumina-run-simulator/releases/download/v0.2.2/illumina-run-simulator-0.2.2-standalone.jar -O illumina-run-simulator.jar
+run_simulator_version=0.2.3
+
+wget https://github.com/dfornika/illumina-run-simulator/releases/download/v${run_simulator_version}/illumina-run-simulator-${run_simulator_version}-standalone.jar -O illumina-run-simulator.jar
