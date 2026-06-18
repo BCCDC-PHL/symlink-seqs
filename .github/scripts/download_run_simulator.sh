53420aa0d0c908f672474b335ab406a7c1107a76
Dan Fornika <dfornika@gmail.com>
2026-06-18 14:43:18 -0700

    Update download link for illumina-run-simulator to v0.2.1

--- .github/scripts/download_run_simulator.sh
+++ .github/scripts/download_run_simulator.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget https://github.com/dfornika/illumina-run-simulator/releases/download/v0.2.0/illumina-run-simulator-0.2.0-standalone.jar -O illumina-run-simulator.jar
+wget https://github.com/dfornika/illumina-run-simulator/releases/download/v0.2.1/illumina-run-simulator-0.2.1-standalone.jar -O illumina-run-simulator.jar
